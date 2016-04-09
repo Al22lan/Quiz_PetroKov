192df25979f288d081f3c30276703b0ef7a3fc7c
Al22lan <ax79@mail.ru>
2016-04-09 11:51:58 +0300

    Fix 1

---
+++ Quiz_PetroKov/MyFirstTableViewController.swift
@@ -0,0 +1,53 @@
+//
+//  MyFirstTableViewController.swift
+//  Quiz_PetroKov
+//
+//  Created by Admin on 09.04.16.
+//  Copyright © 2016 PetroKov_LSR_A. All rights reserved.
+//
+
+import UIKit
+
+class MyFirstTableViewController: UIViewController {
+
+    @IBOutlet weak var TContr: UITableView!
+    var model:[String] = {
+        var aLotOfString = "lghk b d gbi df dfds fd dfv dfv dfv dfv dfv dfv dfvre grtg  lk ;kpwodf;;d os dsfvj kjvh hfdshdfkh kdhvfd vdjkf hkdhv gjisjj  fd  fd fgd dfgdf".componentsSeparatedByString(" ")
+      //  print ( \(aLotofString.count))
+        return aLotOfString
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        TContr.dataSource = self
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension MyFirstTableViewController:UITableViewDataSource{
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print ("tableView cells count")
+        return model.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellId = "StringCell"
+    //  Запрос ячейки у табле вью
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellId)
+        
+        let stringIndex = indexPath.row
+        let section = indexPath.section
+        
+        cell?.textLabel?.text = model[stringIndex]
+        print ("Numbers of Cells \(indexPath)")
+        return cell!
+    }
+    
+}
